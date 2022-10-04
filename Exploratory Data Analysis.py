45df15bc285c458cbe4b38e5c103edd889ade532
Hai Zhu <hz1778@nyu.edu>
2022-10-04 01:58:25 -0400

    Furthur steps on figures.

--- Exploratory Data Analysis.py
+++ Exploratory Data Analysis.py
@@ -3,18 +3,44 @@ import sqlite3
 import seaborn as sns
 import matplotlib.pyplot as plt
 import numpy as np
-from pandas.api.types import is_numeric_dtype, is_string_dtype
 
-#load from db
+# load from db
 data_path = 'musicData.db'
 conn = sqlite3.connect(data_path)
 c = conn.cursor()
-df = pd.read_sql_query("SELECT * FROM musicData", conn)
+df = pd.read_sql_query("SELECT * FROM musicData_clean", conn)
 conn.close()
 print("Finish Loading")
 
-print(df.head())
-print(df.info())
+cat_df = df[['mode', 'music_genre', 'key']]
+num_df = df.drop(['mode', 'music_genre', 'key'], axis=1)
+print(cat_df.head(), num_df.head())
+
+corr = num_df.corr()
+fig1 = plt.figure(figsize=(15, 15))
+sns.heatmap(corr, annot=True, cmap='GnBu', linewidths=0.5)
+sns.set(font_scale=2)
+plt.show()
+fig1.savefig('heatmap.png', dpi=300)
+
+# TODO: figure out how to save the figure
+fig2 = plt.figure(figsize=(30, 30))
+sns.pairplot(num_df, height=5)
+sns.set(font_scale=2)
+plt.tight_layout()
+plt.show()
+#high quality image
+fig2.savefig('pairplot.png', dpi=300)
 
 
-
+fig, ax = plt.subplots(len(cat_df.columns), len(cat_df.columns), figsize=(30, 30))
+for i in range(0, len(cat_df.columns)):
+    primary = cat_df.columns[i]
+    for j in range(0, len(cat_df.columns)):
+        secondary = cat_df.columns[j]
+        if i != j:
+            chart = sns.countplot(x=primary, hue=secondary, data=cat_df, ax=ax[i][j])
+            chart.set_xticklabels(chart.get_xticklabels(), rotation=45)
+plt.show()
+# high quality image
+fig.savefig('cat_df.png', dpi=300)
--- preprocessing.py
+++ preprocessing.py
@@ -10,14 +10,55 @@ df = pd.read_sql_query("SELECT * FROM musicData", conn)
 conn.close()
 print("Finish Loading")
 
-print(df.head(), df.info(), df.describe(),df.isnull().sum())
+print(df.head())
+print(df.info())
+print(df.describe())
+
+# check missing value
 missing_count = df.isnull().sum()
 value_count = df.isnull().count()
-missing_rate = round(missing_count/value_count*100,2)
-missing_df = pd.DataFrame({'missing_count':missing_count,'missing_rate':missing_rate})
+missing_rate = round(missing_count / value_count * 100, 2)
+missing_df = pd.DataFrame({'missing_count': missing_count, 'missing_rate': missing_rate})
 print(missing_df)
+df = df.dropna()
+# replace ? with -1
+df = df.replace('?', -1)
 
-barchart = missing_df.plot.bar(y ="missing_rate")
-for missing_count,missing_rate in enumerate(missing_df['missing_rate']):
-    barchart.text(missing_count,missing_rate,str(missing_rate)+'%',ha='center',va='bottom')
+# name and title to numeric
+df['artist_name_length'] = df['artist_name'].apply(len)
+df['track_name_length'] = df['track_name'].apply(len)
 
+# obtained date remove month
+obtain_date = df['obtained_date'].unique()
+df['obtained_date'] = df['obtained_date'].replace(obtain_date, range(len(obtain_date)))
+
+# mode to dummy code
+df['mode'] = df['mode'].replace(['Major', 'Minor'], [1, 0])
+
+# key to dummy code
+key = df['key'].unique()
+df['key'] = df['key'].replace(key, range(len(key)))
+
+# music genre to numeric
+music_genre = df['music_genre'].unique()
+df['music_genre'] = df['music_genre'].replace(music_genre, np.arange(0, len(music_genre)))
+
+# TEMPO to numeric
+df['tempo'] = df['tempo'].astype(float)
+
+df = df.drop(['artist_name'], axis=1)
+df = df.drop(['track_name'], axis=1)
+df = df.drop(['instance_id'], axis=1)
+df = df.drop(['obtained_date'], axis=1)
+
+print(df.head())
+print(df.info())
+print(df.describe())
+print(df['tempo'].describe())
+
+# to sql
+conn = sqlite3.connect('musicData.db')
+c = conn.cursor()
+df.to_sql('musicData_clean', conn, if_exists='replace', index=False)
+conn.commit()
+conn.close()
